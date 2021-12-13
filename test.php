29b9256ed9bb47f7547cc3d592b394d21f870514
Merlini <merlini.jayson@gmail.com>
2021-12-13 19:42:00 +0100

    Mise au propre snowCrash

--- test.php
+++
@@ -1,7 +0,0 @@
-echo "([x system('getflag')])" > /tmp/regex.php
-
-madirish.net/402
-
-https://crontab.guru/every-2-minutes
-
-https://infosecabsurdity.wordpress.com/2012/12/17/phpwcms-remote-code-execution-and-php-pcre-filter-evasion-bypasses-zeroday/
